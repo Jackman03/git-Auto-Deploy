537d97a98127328ad2339210f7ab52f384395475
JackMan03 <Jacksonvaughn10@gmail.com>
2024-09-23 19:27:30 -0400

    Added gitignore

--- AutoDeploy.sh
+++ AutoDeploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-
-
-
 #read in key and Repo URL from .env
 echo "Running Deploy Script"
+
+#implementation of a try tarch in bash
+if git add .; then
+    echo "files added successfuly"
+
+else
+    echo "git add failed"
+
+fi
